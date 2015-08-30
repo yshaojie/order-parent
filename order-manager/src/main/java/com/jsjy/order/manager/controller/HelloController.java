e05c079f05fc56b70a1548da2018333e7190b13d
shaojieyue <xxdd@gmail.com>
2015-08-30 21:51:28 +0800

    commit

--- order-manager/src/main/java/com/jsjy/order/manager/controller/HelloController.java
+++ order-manager/src/main/java/com/jsjy/order/manager/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.jsjy.order.manager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,9 +13,11 @@ import java.util.Date;
  */
 @Controller
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/order/hello")
     @ResponseBody
     public String helloWorld() {
+        logger.info("request hello.");
         return "hello work. the time:"+new Date();
     }
 }
