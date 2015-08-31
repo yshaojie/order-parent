ebe85a2926d01dce9e0b1a3f713ddc6c96074d4b
shaojieyue <4yxy1986@gmail.com>
2015-08-31 11:43:00 +0800

    update

--- order-manager/src/main/java/com/jsjy/order/manager/controller/HelloController.java
+++ order-manager/src/main/java/com/jsjy/order/manager/controller/HelloController.java
@@ -1,23 +1,23 @@
-package com.jsjy.order.manager.controller;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Date;
-
-/**
- * Created by shaojieyue on 8/30/15.
- */
-@Controller
-public class HelloController {
-    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
-    @RequestMapping("/order/hello")
-    @ResponseBody
-    public String helloWorld() {
-        logger.info("request hello.");
-        return "hello work. the time:"+new Date();
-    }
-}
+//package com.jsjy.order.manager.controller;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.Date;
+//
+///**
+// * Created by shaojieyue on 8/30/15.
+// */
+//@Controller
+//public class HelloController {
+//    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+//    @RequestMapping("/order/hello")
+//    @ResponseBody
+//    public String helloWorld() {
+//        logger.info("request hello.");
+//        return "hello work. the time:"+new Date();
+//    }
+//}
