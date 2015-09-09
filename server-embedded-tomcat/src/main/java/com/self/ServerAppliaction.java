46946696a85df9c7aa9dddedf39b8b3f0aa9f93e
shaojieyue <4yxy1986@gmail.com>
2015-09-09 17:49:29 +0800

    embedded tomcat

---
+++ server-embedded-tomcat/src/main/java/com/self/ServerAppliaction.java
@@ -0,0 +1,30 @@
+package com.self;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import java.io.File;
+
+/**
+ * Created by shaojieyue on 9/9/15.
+ */
+public class ServerAppliaction {
+    public static void main(String[] args) throws ServletException, LifecycleException {
+        String webappDirLocation = "server-embedded-tomcat/src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
