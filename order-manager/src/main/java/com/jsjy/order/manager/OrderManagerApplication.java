16f2b0ebb647b0799ddcd45ed839a0b0a1007760
shaojieyue <4yxy1986@gmail.com>
2015-09-09 11:02:08 +0800

    update

--- order-manager/src/main/java/com/jsjy/order/manager/OrderManagerApplication.java
+++ order-manager/src/main/java/com/jsjy/order/manager/OrderManagerApplication.java
@@ -1,17 +1,16 @@
-//package com.jsjy.order.manager;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-///**
-// * Created by shaojieyue on 8/30/15.
-// */
-//
-//@SpringBootApplication
-//public class OrderManagerApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(OrderManagerApplication.class, args);
-//    }
-//}
+package com.jsjy.order.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by shaojieyue on 8/30/15.
+ */
+
+@SpringBootApplication
+public class OrderManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagerApplication.class, args);
+    }
+}
